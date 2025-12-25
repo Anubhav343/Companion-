f840ca1186ab970dac3d23cb8d0005c7e65e0f2e
Anubhav Dubey <anubhavd2004@gmail.com>
2025-12-25 18:41:02 +0530

    Secret Key no longer available to public

--- Companion/settings.py
+++ Companion/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i*^lf=d_2qs3qmt$&)#2e$un#^h3ey2_sgt2e^uv4w5#opw$x&'
-
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
+    'unsafe-dev-key'
+)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
+
 
 ALLOWED_HOSTS = [
     '127.0.0.1', 
